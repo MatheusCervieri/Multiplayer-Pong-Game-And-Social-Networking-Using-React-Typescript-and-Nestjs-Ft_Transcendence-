f66a6c74d2d995cef677bcae782b5d3133ecd53e
MatheusCervieri <Matheus Cervieri>
2023-02-24 14:00:29 -0300

    I make some improovements handle disconection, but i steel need to fix the time

--- front_transcendence_ts/src/game/GamePauseScream.tsx
+++ front_transcendence_ts/src/game/GamePauseScream.tsx
@@ -10,8 +10,8 @@ export default function GamePauseScream(props:  GamePauseScreamProps) {
     const { gameData } = props;
   return (
     <>
-    <div>{!gameData.player1IsConnected && <p>Player 1 is not connected, he has {gameData.player1PauseTime} seconds to reconnect</p>}</div>
-    <div>{!gameData.player2IsConnected && <p>Player 2 is not connected, he has {gameData.player2PauseTime} seconds to reconnect</p>}</div>
+    <div>{!gameData.player1IsConnected && <p>Player 1 is not connected, he has {Math.floor(gameData.player1PauseTime / 1000)} seconds to reconnect </p>}</div>
+    <div>{!gameData.player2IsConnected && <p>Player 2 is not connected, he has {Math.floor(gameData.player2PauseTime / 1000)} seconds to reconnect </p>}</div>
     </>
     )
 }
