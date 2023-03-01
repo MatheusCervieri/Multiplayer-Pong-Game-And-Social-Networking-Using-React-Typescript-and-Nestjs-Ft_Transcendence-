daa9218c30f3fba6e16208303659551361faa6a4
MatheusCervieri <Matheus Cervieri>
2023-03-01 16:09:51 -0300

    I did the most difficult part of the auth system

--- front_transcendence_ts/src/42api/Authapi.tsx
+++ front_transcendence_ts/src/42api/Authapi.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import  instance from '../confs/axios_information';
 
 export default function Authapi() {
+
+  async function AuthRequest()
+  {
+    //make a request using axios instance
+    try {
+      const response = await instance.get("/auth/42");
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
   return (
-    <div>42api</div>
+    <div>
+      <button onClick={AuthRequest}>Login with 42!</button>
+    </div>
   )
 }
